98599f173e7a15d06d7725d881a4be65e5e3f36f
Intersected2 <brianx8@nycstudents.net>
2026-02-02 12:21:14 -0500

    Update StepTracker.java

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,2 +1,24 @@
-public class StepTracker {
+public class StepTracker{
+	int requirement;
+	double averagesteps;
+	int activedays;
+	int totaldays;
+	int totalsteps;
+	public StepTracker(int requirement){
+		this.requirement = requirement;
+  }
+  public addDailySteps(int steps){
+	  totalsteps += steps;
+  if (steps >= requirement){
+	  activedays++;
+	  totaldays++;
+  } else {
+	  totaldays++
+  }
+  public int activeDays(){
+	  return activedays;
+  }
+  public int averageSteps(){
+	  return totalsteps / totaldays;
+  }
 }
