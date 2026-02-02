de351835d6ee5bf524cc357f0335694c4e3fc0f7
Intersected <brianxu26@gmail.com>
2026-02-02 15:06:30 -0500

    yes

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,24 +1,30 @@
 public class StepTracker{
 	int requirement;
-	double averagesteps;
-	int activedays;
-	int totaldays;
-	int totalsteps;
+	double averagesteps = 0;
+	int activedays = 0;
+	double totaldays = 0;
+	double totalsteps = 0;
 	public StepTracker(int requirement){
 		this.requirement = requirement;
   }
-  public addDailySteps(int steps){
+  public void addDailySteps(int steps) {
 	  totalsteps += steps;
-  if (steps >= requirement){
-	  activedays++;
-	  totaldays++;
-  } else {
-	  totaldays++
+	  if (steps >= requirement) {
+		  activedays++;
+		  totaldays++;
+	  } else {
+		  totaldays++;
+	  }
   }
   public int activeDays(){
 	  return activedays;
   }
-  public int averageSteps(){
-	  return totalsteps / totaldays;
+  public double averageSteps(){
+		if (totaldays != 0){
+			averagesteps = totalsteps / totaldays;
+			return averagesteps;
+		} else{
+			return 0.0;
+		}
   }
 }
