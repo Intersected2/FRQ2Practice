59dfeda22c339295b304b6be9e2c4bcf10f0057a
Intersected2 <brianx8@nycstudents.net>
2026-02-03 23:29:45 -0500

    idk how to solve

--- src/HiddenWord.java
+++ src/HiddenWord.java
@@ -1,22 +1,20 @@
 public class HiddenWord{
     private String word = "";
-    private String ram = "";
+    private String  ram = "";
+    private String ram2 = "";
     public HiddenWord(String word){
         this.word = word;
     }
     public String getHint(String gword){
-        ram = "";
-        for (int i = 0;i < word.length(); i++){
+        for (int i = 0; i < word.length();i++){
             if (word.substring(i,i + 1).equals(gword.substring(i,i + 1))){
                 ram += word.substring(i,i + 1);
-            }else if (true){
-                for (int j = 0; j < word.length(); j++){
-                    if (word.substring(i,i + 1).equals(word.substring(j,j + 1))){
-                        ram += "+";
+            }else {
+                for (int j = 0; j < gword.length();j++){
+                    if (word.substring(i,i + 1).equals(gword.substring(j,j + 1)) && (i != j)){
+
                     }
                 }
-            }else{
-                ram += "*";
             }
         }
         return ram;
